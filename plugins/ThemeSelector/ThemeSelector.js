334fecbcad2fea2155ddfa1a3dabb649cab2ad5c
chuck911 <chuck911@126.com>
2010-11-16 23:51:30 +0800

    ThemeSelector ing

--- plugins/ThemeSelector/ThemeSelector.js
+++ plugins/ThemeSelector/ThemeSelector.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-    alert('ok');    
+    $('#Theme14').click(function(){
+        $('body').css('background-image','url("http://static.leihou.com/v319001/images/themes/bg-14.gif")')
+        .css('background-repeat','repeat')
+        .css('background-attachment','scroll')
+        .css('background-color','rgb(19, 21, 22)')
+        .css('rgb(51, 51, 51)');
+    });
 });
