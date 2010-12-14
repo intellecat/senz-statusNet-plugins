96f9e996aebaf43f31df8c9738782b34a4b01841
chuck911 <chuck911@126.com>
2010-12-14 18:06:00 +0800

    style updated and locale file fixed

--- plugins/CommonStyle/commonstyle.js
+++ plugins/CommonStyle/commonstyle.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
-    $('#aside_primary').height($('#core').height());
-
-    //$('.section h2').addClass('collapsible');
-    //$('.section h2').click(function(){
-    //    $(this).nextAll().slideToggle();
-    //    $(this).toggleClass('collapsed');
-    //});
+    $(document).ready(function(){
+        var contentPadding = parseInt($('#content').css('padding-top'))+parseInt($('#content').css('padding-bottom'))+parseInt($('#content').css('border-top-width'))+parseInt($('#content').css('border-bottom-width'));
+        var contentHeight = $('#content').height() + contentPadding;
+        var sideHeight = $('#aside_primary').height();
+        var coreHeight = $('#core').height();
+        if(sideHeight < coreHeight){
+            $('#aside_primary').height(coreHeight);
+        }else if(contentHeight < coreHeight){
+            $('#content').height(coreHeight - contentPadding);
+        }
+    });
 });
